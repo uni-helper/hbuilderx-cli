3cfe7237a176223e3c8bca67fad5e2b5a9e95daf
KeJunMao <kejun1997@gmail.com>
2023-04-24 09:39:12 +0800

    feat: add project action

---
+++ src/actions/project.ts
@@ -0,0 +1,39 @@
+import { $ } from 'execa'
+import type { Options } from '../types'
+import { decodeGbk, parseArgs } from '../utils'
+
+export interface IProjectOpen {
+  path: string
+}
+
+export interface IProjectClose {
+  path: string
+}
+
+export function createProjectContext(options: Options) {
+  const cli = options.cli
+  return {
+    list() {
+      const { stdout } = $({
+        encoding: null,
+      }).sync`${cli} project list`
+      const output = decodeGbk(stdout)
+      const matches = output.matchAll(/\d\s-\s(.*?)\s/gm)
+      return [...matches].map(match => match[1])
+    },
+    open(args: IProjectOpen) {
+      const { stdout } = $({
+        encoding: null,
+      }).sync`${cli} project open ${parseArgs(args)}`
+      const output = decodeGbk(stdout)
+      return output.includes('项目导入成功')
+    },
+    close(args: IProjectClose) {
+      const { stdout } = $({
+        encoding: null,
+      }).sync`${cli} project close ${parseArgs(args)}`
+      const output = decodeGbk(stdout)
+      return output.startsWith('项目关闭完成')
+    },
+  }
+}
--- test/actions/project.test.ts
+++ test/actions/project.test.ts
@@ -1,7 +1,28 @@
-import { describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { normalizePath } from 'vite'
+import { resolveOptions } from '../../src/options'
+import { createProjectContext } from '../../src/actions/project'
+
+const options = resolveOptions({})
 
 describe('project action', () => {
+  const project = createProjectContext(options)
   it('list', () => {
+    const list = project.list()
+    expect(list).toBeInstanceOf(Array)
+  })
 
+  it('open', () => {
+    const isOpen = project.open({
+      path: normalizePath(process.cwd()),
+    })
+    expect(isOpen).toBe(true)
+  })
+
+  it('close', () => {
+    const isClosed = project.close({
+      path: normalizePath(process.cwd()),
+    })
+    expect(isClosed).toBe(true)
   })
 })
